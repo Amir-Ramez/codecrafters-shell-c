dfdf0260e8e083df5878d1f89c7854ff3452a3e3
Amir-Ramez <amirrelias@outlook.com>
2026-03-12 15:56:26 +0000

    Implement type

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
+    char *builtin_commands[] = {"echo", "exit", "type", NULL};
     while (1) {
         printf("$ ");
 
@@ -17,6 +18,21 @@ int main(int argc, char *argv[]) {
             break;
         else if (strncmp(command, "echo ", 5) == 0) {
             printf("%s\n", command + 5);
+        } else if (strncmp(command, "type ", 5) == 0) {
+            char *type = command + 5;
+
+            int found = 0;
+            for (int i = 0; builtin_commands[i] != NULL; ++i) {
+                if (strcmp(builtin_commands[i], type) == 0) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            if (found)
+                printf("%s is a shell builtin\n", type);
+            else
+                printf("%s: not found\n", type);
         } else {
             printf("%s: command not found\n", command);
         }
